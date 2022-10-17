67ba6d3898f276e9bf713d95eb16a6b61e3c238c
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-17 22:07:34 +0300

    Initial Commit

---
+++ 010_Onboarding/_10_OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _10_OnboardingApp.swift
+//  010_Onboarding
+//
+//  Created by nikita on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _10_OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
